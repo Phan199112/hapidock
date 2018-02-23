08a9816e49856d679e58559fb681a7c52794b50f
Paul <paul@crowleymarine.com>
2018-02-23 09:47:06 -0500

    Updated Lima IP address

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const provision = async () => {
 
         // IP Address Auth
         // @TODO - Pull the IP Addresses from Oracle
-        await server.auth.strategy('admin_ips', 'ip-whitelist', ['179.7.148.90','85.88.162.16','190.237.78.74']);
+        await server.auth.strategy('admin_ips', 'ip-whitelist', ['179.7.148.90','85.88.162.16','190.234.11.171']);
 
         // await server.auth.default('jwt');
 
