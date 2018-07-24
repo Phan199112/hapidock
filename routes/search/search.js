587f40dcefa5caa269551e8622e97588e9a13b47
Paul Crowley <paul@crowleymarine.com>
2018-07-24 08:56:34 -0600

    Added ability to limit response by index

--- routes/search/search.js
+++ routes/search/search.js
@@ -15,6 +15,7 @@ module.exports = [
 					},
 					query: {
 						debug: Joi.boolean().description('Debug mode'),
+						index: Joi.string().valid(['models','products','pages']).description('Index to return'),
 						page_number: Joi.number().default(1).description('Page number')
 					}
 				}
@@ -43,61 +44,88 @@ module.exports = [
 async function search(request, reply) {
 
 	const client = request.es;
+	const index = request.query.index;
 
 	try {
 
+		// models query
+		const models_query = [
+			{ index: 'models', type: 'models_es' },
+	    	{
+	    	'query': {
+					'multi_match': {
+		        'query': request.params.search_parameter,
+		        'fields': ['mfg_name', 'product_line', 'year', 'engine', 'model'],
+		        'type': 'most_fields'
+		    	}
+				},
+					'from': request.query.page_number - 1,
+		    	'size': 10
+			},
+			{ index: 'serials', type: 'serials_es' },
+	    	{
+	    	'query': {
+					'multi_match': {
+		        'query': request.params.search_parameter,
+		        'fields': ['serial_number'],
+		        'type': 'most_fields'
+		    	}
+				},
+					'from': request.query.page_number - 1,
+		    	'size': 10
+			}
+		];
+
+		// products query
+		const products_query = [
+			{ index: 'products', type: 'products_es' },
+	    	{
+	    	'query': {
+					'multi_match': {
+		        'query': request.params.search_parameter,
+		        'fields': ['sku^2', 'superceding_products.sku^2', 'interchange.sku^2'],
+		        'type': 'most_fields'
+		    	}
+				},
+					'from': request.query.page_number - 1,
+		    	'size': 10
+			}
+		]
+
+		// pages query
+		const pages_query = [
+			{ index: 'pages', type: 'pages_es' },
+	    	{
+	    	'query': {
+					'multi_match': {
+		        'query': request.params.search_parameter,
+		        'fields': ['diagram.alt_title', 'diagram.notes', 'diagram.page_title', 'diagram.result', 'parts.footnote_text', 'parts.sku^2', 'parts.partno', 'parts.name^2'],
+		        'type': 'most_fields'
+		    	}
+				},
+					'from': request.query.page_number - 1,
+		    	'size': 10
+			}
+		]
+
+		// Combine search queries
+		let es_query = []
+		if (index == 'models') {
+			es_query = models_query;
+		}
+		else if (index == 'products') {
+			es_query = products_query;
+		}
+		else if (index == 'pages') {
+			es_query = pages_query;
+		}
+		else {
+			es_query = models_query.concat( products_query, pages_query );
+		}
+
 		// Send Elasticsearch request
 		const response = await client.msearch({
-			body: [
-				{ index: 'models', type: 'models_es' },
-		    {
-		    	'query': {
-						'multi_match': {
-			        'query': request.params.search_parameter,
-			        'fields': ['mfg_name', 'product_line', 'year', 'engine', 'model'],
-			        'type': 'most_fields'
-			    	}
-					},
-					'from': request.query.page_number - 1,
-		    	'size': 10
-				},
-				{ index: 'serials', type: 'serials_es' },
-		    {
-		    	'query': {
-						'multi_match': {
-			        'query': request.params.search_parameter,
-			        'fields': ['serial_number'],
-			        'type': 'most_fields'
-			    	}
-					},
-					'from': request.query.page_number - 1,
-		    	'size': 10
-				},
-				{ index: 'products', type: 'products_es' },
-		    {
-		    	'query': {
-						'multi_match': {
-			        'query': request.params.search_parameter,
-			        'fields': ['sku^2', 'superceding_products.sku^2', 'interchange.sku^2'],
-			        'type': 'most_fields'
-			    	}
-					},
-					'from': request.query.page_number - 1,
-		    	'size': 10
-				},
-				{ index: 'pages', type: 'pages_es' },
-		    {
-		    	'query': {
-						'multi_match': {
-			        'query': request.params.search_parameter,
-			        'fields': ['diagram.alt_title', 'diagram.notes', 'diagram.page_title', 'diagram.result', 'parts.footnote_text', 'parts.sku^2', 'parts.partno', 'parts.name^2'],
-			        'type': 'most_fields'
-			    	}
-					},
-					'from': request.query.page_number - 1,
-		    	'size': 10
-				}
-			]
+			body: es_query
 		})
 
 		// Clean up response object
@@ -111,15 +139,16 @@ async function search(request, reply) {
 		  searchResults = r.hits.hits.map(result => result['_source']);
 
 		  // models
-		  if (i == 0 || i == 1) {
+		  // Concatenate the 'models' and 'serials' response
+		  if ((!index && (i == 0 || i == 1)) || index == 'models') {
 		  	results['models'] = results['models'].concat(searchResults);
 		  }
 		  // products
-		  if (i == 2) {
+		  if ((!index && i == 2) || (index == 'products' && i == 0)) {
 		  	results['products'] = searchResults;
 		  }
 		  // pages
-		  if (i == 3) {
+		  if ((!index && i == 3) || (index == 'pages' && i == 0)) {
 		  	results['pages'] = searchResults;
 		  }
 		}
